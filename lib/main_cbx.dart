cd4d00819d1afd9441452a457865baa5c91089aa
dinesh parmar <shashikha1000@gmail.com>
2021-09-22 18:43:04 +0530

    fresh apk no changes done yet

--- lib/main_cbx.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'app_config.dart';
-
-void main() async {
-  AppConfig devAppConfig = AppConfig(appName: 'CBX', versions: 1.3);
-  Widget app = await initializeApp(devAppConfig);
-  runApp(app);
-}
